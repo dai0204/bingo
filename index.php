a6a8af893048b90d3a0979036abe0333abcce8b1
dai0204 <nandai0204@gmail.com>
2019-06-29 13:18:10 +0900

    edit

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+/*
+$nums[][]
+B: $nums[0] 1-15
+I: $nums[1] 16-30 
+..
+*/
+$nums = [];
+
+for ($i = 0; $i < 5; $i++){
+  $col = range($i * 15 + 1, $i * 15 + 15);
+  shuffle($col);
+  $nums[$i] = array_slice($col, 0, 5);
+}
+
+$nums [2][2] = "FREE";
+// var_dump($nums);
+// exit;
+
+function h($s){
+  return htmlspecialchars($s,ENT_QUOTES, 'UTF-8');
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,13 +36,13 @@
       <tr>
         <th>B</th> <th>I</th> <th>N</th> <th>G</th> <th>O</th>
       </tr>
+      <?php for ($i = 0; $i < 5; $i++) : ?>
       <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
+        <?php for ($j = 0; $j < 5; $j++) : ?>
+        <td><?= h($nums[$j][$i]); ?></td>
+        <?php endfor; ?>
       </tr>
+      <?php endfor; ?>
     </table>
   </div>
 </body>
